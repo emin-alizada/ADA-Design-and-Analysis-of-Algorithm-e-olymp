dfabf739b5a2196ac60f49c24db090d8ed4bdb84
Emin <emin.134.134@gmail.com>
2021-09-26 18:40:27 +0400

    6122 c++

---
+++ 318 - not working/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+
+using namespace std;
+
+
+int main() {
+    int t;
+    vector<vector<long long>> c;
+
+    cin >> t;
+
+//    while (t--) {
+//        long long n, k;
+//
+//        cin >> n >> k;
+//
+//        cout << binomCoef(n, k) << endl;
+//    }
+
+    return 0;
+}
---
+++ 318 - not working/script.js
@@ -0,0 +1,34 @@
+const c = [];
+
+function binomCoef(n, k) {
+    if (n === k || k === 0) {
+        return 1;
+    }
+
+    const left = c[n - 1]?.[k - 1] ? c[n - 1]?.[k - 1] : binomCoef(n - 1, k - 1);
+    const right = c[n - 1]?.[k] ? c[n - 1]?.[k] : binomCoef(n - 1, k);
+
+    return left + right;
+}
+
+const fs = require('fs')
+
+fs.readFile('input.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+
+    const lines = data.split("\n");
+
+    const testNumbers = lines[0];
+    const answers = []
+
+    for (let i = 1; i <= testNumbers; i++) {
+        const [n,k] = lines[i].split(" ");
+
+        answers.push(binomCoef(parseInt(n), parseInt(k)))
+    }
+
+    console.log(answers.join("\n"));
+})
