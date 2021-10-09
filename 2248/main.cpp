e027a7008055b59c3629ff4d73eb406b158aedb7
Emin <emin.134.134@gmail.com>
2021-10-09 10:06:15 +0400

    2248 c++

--- 2248/main.cpp
+++ 2248/main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+
+    int a[n];
+
+    int min = INT32_MAX;
+    int index = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+
+        int rangeBegin = i - k + 1;
+        if (index < rangeBegin)
+        {
+            min = a[rangeBegin];
+            for (int j = rangeBegin; j <= i; j++)
+                if (min >= a[j]) {
+                    min = a[j];
+                    index = j;
+                }
+        }
+        if (min >= a[i]){
+            min = a[i];
+            index = i;
+        }
+
+        if (i + 1 >= k) {
+            cout << min << " ";
+        }
+    }
 }
