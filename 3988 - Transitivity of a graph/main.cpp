fc8e3df8fd9a3cdf9d2df74bc094bc4ebaaf9a98
Emin <emin.134.134@gmail.com>
2021-12-11 20:16:00 +0400

    removed todo

--- 3988 - Transitivity of a graph/main.cpp
+++ 3988 - Transitivity of a graph/main.cpp
@@ -8,12 +8,15 @@ int n, m;
 bool g[MAX][MAX];
 
 bool floyd() {
-//    TODO analyze again
         for (int i = 1; i <= n; i++) {
             for (int j = i + 1; j <= n; j++) {
                 for (int k = 1; k <= n; k++) {
                     if (k == i || k == j) continue;
 
+// Here we check transitivity,
+// like if there is edge from i to k,
+// and from k to j, there has to be direct edge from i to j as well
+// otherwise this graph is not transitive
 
                 if (g[i][k] && g[k][j]) {
                     if(!g[i][j]) {
