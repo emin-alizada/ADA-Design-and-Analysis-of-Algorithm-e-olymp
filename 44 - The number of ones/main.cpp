78a4908cb476547a17c6fb0c5fe1b9edeac67b9a
Emin <emin.134.134@gmail.com>
2021-12-11 14:02:59 +0400

    44 another solution c++

--- 44 - The number of ones/main.cpp
+++ 44 - The number of ones/main.cpp
@@ -48,3 +48,34 @@ int main() {
     cout << d[n];
     return 0;
 }
+
+
+
+// Another simpler solution
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//    int n;
+//    cin >> n;
+//
+//    int dp[n+1];
+//
+//    dp[1] = 1;
+//    dp[2] = 2;
+//    for (int i = 3; i <= n; i++) {
+//        dp[i] = i;
+//        for (int j = 1; j < i; j++) {
+//            dp[i] = min(dp[i], dp[j] + dp[i-j] );
+//            if (i%j == 0) {
+//                dp[i] = min(dp[i], dp[j] + dp[i/j]);
+//            }
+//        }
+//    }
+//
+//    cout << dp[n];
+//
+//    return 0;
+//}
