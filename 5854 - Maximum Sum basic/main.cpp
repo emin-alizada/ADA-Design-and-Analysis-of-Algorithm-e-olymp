681c88d68a1d3f58ac0fc0b4f9a7930427c6caef
Emin <emin.134.134@gmail.com>
2021-12-11 14:17:33 +0400

    5854 another solution c++

--- 5854 - Maximum Sum basic/main.cpp
+++ 5854 - Maximum Sum basic/main.cpp
@@ -7,44 +7,51 @@
 //
 
 #include <iostream>
-//#include <bits/stdc++.h>  //need to write in e-olymp, no need in XCODE
+
 using namespace std;
 
-// Week 9: November 17 - Dynamic Programming
-int main(int argc, const char * argv[]) {
-    int n,m;
-    cin>>n>>m;
-    int a[n][m];
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<m; j++) {
-            cin>>a[i][j];
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    int dp[n][m];
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cin >> dp[i][j];
         }
     }
 
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<m; j++) {
-            if(i==0){
-                continue;
-            }
-            else if(j==0){
-                a[i][j]+=max(a[i-1][j],a[i-1][j+1]);
-            }
-            else if(j==m-1){
-                a[i][j]+=max(a[i-1][j],a[i-1][j-1]);
-            }
-            else{
-                a[i][j]+=max(max(a[i-1][j],a[i-1][j-1]),a[i-1][j+1]);
-            }
-        }
-    }
-    long long max=LONG_MIN;
+    for (int i = 1; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            int maxPrev = INT32_MIN;
+            maxPrev = max(maxPrev, dp[i-1][j]);
 
-    for (int j=0; j<m; j++) {
-        if(a[n-1][j]>=max){
-            max=a[n-1][j];
+            if (j > 0) {
+                maxPrev = max(maxPrev, dp[i-1][j-1]);
+            }
+
+            if (j < m-1) {
+                maxPrev = max(maxPrev, dp[i-1][j+1]);
+            }
+
+            dp[i][j] += maxPrev;
         }
     }
 
-    cout<<max;
+//    for (int i = 0; i < n; ++i) {
+//        for (int j = 0; j < m; ++j) {
+//            cout << dp[i][j] << " ";
+//        }
+//        cout << endl;
+//    }
+
+    int maxResult = INT32_MIN;
+    for (int j = 0; j < m; ++j) {
+        maxResult = max(maxResult, dp[n - 1][j]);
+    }
+
+    cout << maxResult;
+
     return 0;
 }
